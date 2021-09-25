e8374e76d44e45c37c1d5ff7af9d1c4142209e78
amber renfro <amber.renfro2021@gmail.com>
2021-09-25 04:34:03 -0500

    added register and login functionality along with token handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import {useState, useEffect} from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavMenu from './components/NavMenu/NavMenu';
 import Home from './components/Home/Home';
@@ -8,10 +9,22 @@ import WatchList from './components/WatchList/WatchList';
 import SignIn from './components/SignIn/SignIn';
 
 function App() {
+  const [token, settoken] = useState('');
+  useEffect(() => {
+    if(localStorage.getItem('token')){
+      settoken(localStorage.getItem('token'));
+    }
+  }, [])
+  const updateToken = (newToken) => {
+    localStorage.setItem('token', newToken);
+    settoken(newToken);
+    console.log(token);
+  }
+
   return (
     <div className='App'>
       <Router>
-        <NavMenu />
+        <NavMenu updateToken={updateToken} token={token} />
         <Switch>
           <Route exact path='/'>
             <Home />
@@ -23,7 +36,7 @@ function App() {
             <WatchList />
           </Route>
           <Route exact path='/sign-in'>
-            <SignIn />
+            <SignIn updateToken={updateToken} token={token} />
           </Route>
         </Switch>
       </Router>
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import { Container } from 'reactstrap';
+import React, {useState} from 'react';
+import { Container, Button, FormGroup, Form, Label, Input } from 'reactstrap';
 // comment to push branch to remote repository
 
-const SignIn = () => {
+const SignIn = (props) => {
+  const [usernameSignup, setUsernameSignup] = useState("");
+  const [passwordSignup, setPasswordSignup] = useState("");
+  const [usernameLogin, setUsernameLogin] = useState("");
+  const [passwordLogin, setPasswordLogin] = useState("");
+
+  let handleSubmitSignup= (event) => {
+    event.preventDefault();
+    fetch("http://localhost:4000/user/register", {
+      method: 'POST',
+      body: JSON.stringify({username: usernameSignup, password: passwordSignup}),
+      headers: new Headers ({
+        'Content-Type': 'application/json'
+      })
+    }).then(
+      (response) => response.json()
+    ).then((data) => {
+      console.log(data);
+      props.updateToken(data.token)
+    })
+  }
+
+  let handleSubmitLogin= (event) => {
+    event.preventDefault();
+    fetch("http://localhost:4000/user/login", {
+      method: 'POST',
+      body: JSON.stringify({username: usernameLogin, password: passwordLogin}),
+      headers: new Headers ({
+        'Content-Type': 'application/json'
+      })
+    }).then(
+      (response) => response.json()
+    ).then((data) => {
+      console.log(data);
+      props.updateToken(data.token)
+    })
+  }
+
   return (
     <>
       <Container className='mt-5'>
-        <h1>Register or Log In</h1>
+           <h1>Sign Up</h1>
+            <Form onSubmit={handleSubmitSignup}>
+                <FormGroup>
+                    <Label htmlFor="username">Username</Label>
+                    <Input onChange = {(e) => setUsernameSignup(e.target.value)} name="username" value={usernameSignup}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="password">Password</Label>
+                    <Input onChange = {(e) => setPasswordSignup(e.target.value)} name="password" value={passwordSignup}/>
+                </FormGroup>
+                <Button type="submit">Sign Up</Button>
+            </Form>
+          <h1>Log In</h1>
+            <Form onSubmit={handleSubmitLogin}>
+                <FormGroup>
+                    <Label htmlFor="username">Username</Label>
+                    <Input onChange = {(e) => setUsernameLogin(e.target.value)} name="username" value={usernameLogin}/>
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="password">Password</Label>
+                    <Input onChange = {(e) => setPasswordLogin(e.target.value)} name="password" value={passwordLogin}/>
+                </FormGroup>
+                <Button type="submit">Login</Button>
+            </Form>
       </Container>
     </>
   );
